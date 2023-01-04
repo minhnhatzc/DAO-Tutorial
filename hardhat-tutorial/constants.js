44dff461c61c42e6daa4c5c2d91373e1cec8c9ca
Pham Van Nhat <114339708+minhnhatzc@users.noreply.github.com>
2023-01-04 10:26:45 +0700

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x094A2f3390502c8Ebe3A8c6cF4D17fE447f96E0a";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
